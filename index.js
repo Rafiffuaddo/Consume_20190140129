b3d5d85cfe9b38fb6356dd30019c4a944dd556cf
rafiffuaddo <rafiffuad3108@gmail.com>
2022-01-13 14:27:31 +0700

    ok

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ $("#dataTable").ready(function () {
 
         
             console.log(response[i])
-            td1.innerHTML = response[i]['id'];
+            td1.innerHTML = response[i]['id_Obat'];
             td2.innerHTML = response[i]['Nama_Obat'];
             td3.innerHTML = response[i]['Harga'];
             td4.innerHTML = response[i]['Expired'];
